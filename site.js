a6e0790bb70f568f4b3e9e265051b2bd6d32f1a5
AARON AXIEL SELENER <48522418@ort>
2025-03-13 12:06:25 -0300

    ayuda manu

--- site.js
+++ site.js
@@ -1,3 +1,6 @@
 function ValidarForm(){
-
+    let nombre = document.getElementById('Nombre');
+    let email = document.getElementById('Email');
+    let contraseña = document.getElementById('Contraseña');
+    let msj = document.getElementById('confirmation-message');
 }
