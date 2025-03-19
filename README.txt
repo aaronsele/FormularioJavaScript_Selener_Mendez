8c152d88a26e183cd4fcc4eff1ed2f28a8e1d2da
AARON AXIEL SELENER <48522418@ort>
2025-03-19 10:08:50 -0300

    fin

---
+++ README.txt
@@ -0,0 +1,7 @@
+1) para la interfaz del formulario pensamos algo basico para no complicarnos ya que no llegabamos con el tiempo. elegimos colores simples y le añadimos un hover al boton de enviar
+
+2) hay una validacion para cada campo, que chequea si esta bien. si no lo esta, hace la variable "x" falsa asi no se envia el form y manda el mensaje de error
+
+3) nos costaba revisar el email y que la contraseña contenga letra y numero. tambien sacar el requires fue un problema, pero lo pudimos resolver con tiempo
+
+4) si hubiesemos tenido mas tiempo podriamos haber mejorado el css y añadir validacion al instante, no solo cuando se envia
--- site.js
+++ site.js
@@ -1,6 +1,49 @@
-function ValidarForm(){
-    let nombre = document.getElementById('Nombre');
-    let email = document.getElementById('Email');
-    let contraseña = document.getElementById('Contraseña');
-    let msj = document.getElementById('confirmation-message');
-}
+document.getElementById("formu").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let nombre = document.getElementById("nombre");
+    let email = document.getElementById("email");
+    let contra = document.getElementById("contra");
+    let contra2 = document.getElementById("contra2");
+
+    let x = true;
+
+    
+    if (nombre.value.length < 3) {
+        document.getElementById("errornombre").textContent = "el nombre tiene que tener 3 letras minimo";
+        x = false;
+    } else {
+        document.getElementById("errornombre").textContent = "";
+    }
+
+   
+    let emailpre = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailpre.test(email.value)) {
+        document.getElementById("erroremail").textContent = "pone un email valido";
+        x = false;
+    } else {
+        document.getElementById("erroremail").textContent = "";
+    }
+
+    
+    let contraRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (!contraRegex.test(contra.value)) {
+        document.getElementById("errorcontra").textContent = "la contraseña tiene que tener 8 caracteres y una letra y un numero minimo";
+        x = false;
+    } else {
+        document.getElementById("errorcontra").textContent = "";
+    }
+
+    
+    if (contra.value !== contra2.value) {
+        document.getElementById("errorcontra2").textContent = "Las contraseñas no coinciden.";
+        x = false;
+    } else {
+        document.getElementById("errorcontra2").textContent = "";
+    }
+
+   
+    if (x) {
+        alert("Formulario enviado correctamente.");
+    }
+});
